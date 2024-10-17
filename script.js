c54aee74e182bd3f1d7decd7150ecbf5eab01231
Aaditya Kumar Mittal <adityakumarashish1909@gmail.com>
2024-10-18 01:59:22 +0530

    Completed React Components and Multiple Component Rendering

--- script.js
+++ script.js
@@ -9,22 +9,28 @@ if (module.hot) {
 }
 
 // const h1 = <h1>My React Element</h1>
-function Card(key, productName, productImage) {
-  return <div className="card inter-family" key={key}>
+function Card(props) {
+  //Destructuring the object
+  // const { key, title, src } = props;
+  const { title, src, price } = props;
+
+  // return <div className="card inter-family" key={key}>
+  return <div className="card inter-family">
     {/* <img src="https://www.apple.com/in/iphone-16-pro/images/overview/product-viewer/iphone-pro/all_colors__fdpduog7urm2_xlarge.jpg"></img> */}
-    <img src={productImage}></img>
-    <h2>{productName}</h2>
+    <img src={src}></img>
+    <h2>{title}</h2>
     <h2>Ratings: ⭐⭐⭐⭐⭐</h2>
-    <h3>Price - $1500</h3>
+    <h3>Price - ${price}</h3>
   </div >
 }
 
-// const container = []
+//Creating a function for type 
+function Card2() {
+  return <h1>Hello World!</h1>
+}
 
-// for (let index = 0; index < 21; index++) {
-//   container.push(Card(index));
-// }
-
+//Demonstrationg Use of Fetch API
+/*
 fetch('https://dummyjson.com/products')
   .then(res => res.json())
   .then((data) => {
@@ -32,7 +38,9 @@ fetch('https://dummyjson.com/products')
     console.log(data)
     const container2 = data.products.map((product) => {
       // console.log(products);
-      return Card(product.id, product.title, product.images[0]);
+      return Card(
+
+        { title: product.title, key: product.id, src: product.images[0] });
     });
 
     const containerDiv = <div className="container">{container2}</div>
@@ -40,10 +48,81 @@ fetch('https://dummyjson.com/products')
     const root = createRoot(document.getElementById('root'))
     root.render(containerDiv)
   });
+*/
+
+//Demonstrationg Use of Fetch API using React Custom Component
+
+fetch('https://dummyjson.com/products')
+  .then(res => res.json())
+  .then((data) => {
+    // Map over products and render each as a Card component
+    const container2 = data.products.map((product) => (
+      <Card key={product.id} price={product.price} title={product.title} src={product.images[0]} />
+    ));
+
+    // Wrap all cards in a container div
+    const containerDiv = <div className="container">{container2}</div>;
+
+    // Render the container div into the DOM
+    const root = createRoot(document.getElementById('root'));
+    root.render(containerDiv);
+  })
+  .catch((err) => console.error("Error fetching data:", err));
+
+
+
+
+//const root = createRoot(document.getElementById('root'))
+
+//root.render() method takes a react element
+//Rendering a React Element of String Type
+/*
+root.render(
+  //Reacts Mandotory Properties
+  {
+    $$typeof: Symbol.for('react.element'),
+    type: 'h1',
+    ref: null,
+    props: {
+      children: 'Hello World!',
+    },
+  }
+);
+*/
+
+//Rendering a React Element whose Type is a Function --> a Component basically
+/*
+root.render({
+  $$typeof: Symbol.for('react.element'),
+  //setting the type to Card2 we passed the complete Card2 function to the render method
+  type: Card,
+  ref: null,
+  props: {
+    key: 1,
+    title: 'Apple',
+    src: 'https://cdn.dummyjson.com/products/images/groceries/Apple/1.png',
+  },
+})
+  */
+
+
+//Rendering a React Component Using React.createElement()
+/*
+root.render(React.createElement(Card, {
+  key: 1,
+  title: 'Apple',
+  src: 'https://cdn.dummyjson.com/products/images/groceries/Apple/1.png',
+}))
+
+console.log(React.createElement(Card, {
+  key: 1,
+  title: 'Apple',
+  src: 'https://cdn.dummyjson.com/products/images/groceries/Apple/1.png',
+}));
+*/
+
+//Render a React Component --> This is a Custom Component
+// root.render(<Card title='Apple'
+//   src='https://cdn.dummyjson.com/products/images/groceries/Apple/1.png' />)
 
 // console.log(React);
-// console.log(user.name);
-// console.log("Hello World!!!!!!!!!!!");
-//create our react element
-
-
